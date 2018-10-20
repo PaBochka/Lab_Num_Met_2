c407d0843c6e7ef148fb70e6922309be3e96660f
PaBochka <bochenkovp@yandex.ru>
2018-10-20 21:53:31 +0300

    V.2 (task chemical reaction)

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import sys
 import math
+import math_part
 # Импортируем наш интерфейс из файла
 from Form import *
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from MyMplCanc import MtMplCanv
-import math_part
 from numpy import float64
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from PyQt5 import QtWidgets, QtGui, QtCore
@@ -24,31 +24,21 @@ class MyWin(QMainWindow, Ui_MainWindow):
 
         # добавление шаблона размещения на виджет
         self.companovka_for_mpl = QtWidgets.QVBoxLayout(self.widget)
-
-        self.companovka_for_mpl2 = QtWidgets.QVBoxLayout(self.widget_2)
-
         # получение объекта класса холста с нашим рисунком
 
-        self.canvas = MtMplCanv(self.figure)
-
-        self.canvas2 = MtMplCanv2(self.figure2)
+        self.canvas = MtMplCanv(self.figure)        
         # Размещение экземпляра класса холста в шаблоне размещения
-        self.companovka_for_mpl.addWidget(self.canvas)
-
-        self.companovka_for_mpl2.addWidget(self.canvas2)
+        
+        self.companovka_for_mpl.addWidget(self.canvas)        
         # получение объекта класса панели управления холста
-        self.toolbar = NavigationToolbar(self.canvas, self)
-
-        self.toolbar2 = NavigationToolbar(self.canvas2, self)
+        
+        self.toolbar = NavigationToolbar(self.canvas, self)        
         # Размещение экземпляра класса панели управления в шаблоне размещения
-        self.companovka_for_mpl.addWidget(self.toolbar)
-
-        self.companovka_for_mpl2.addWidget(self.toolbar2)
+        
+        self.companovka_for_mpl.addWidget(self.toolbar)        
         # Здесь прописываем событие нажатия на кнопку
+        
         self.pushButton.clicked.connect(self.MyFunction)
-
-    # Пока пустая функция которая выполняется
-    # при нажатии на кнопку
     def MyFunction(self):
         p = float64(self.textEdit.toPlainText())
         v = float64(self.textEdit_2.toPlainText())
@@ -64,9 +54,9 @@ class MyWin(QMainWindow, Ui_MainWindow):
         d = float64(self.textEdit_9.toPlainText())
         h = float64(self.textEdit_11.toPlainText())
         
-        #self.sec_win = second_window(self)
-        math_part.mathpart.building(self, p, v, y, k, c, u10, u20, eps, d, x0, h)
-        #self.sec_win.show()
+        self.sec_win = second_window(self)
+        math_part.mathpart.building(self, p, v, y, k, c, u10, u20, eps, d, x0, h, self.sec_win)
+        self.sec_win.show()
 
 class second_window(QMainWindow, Ui_MainWindow_tab):
     def __init__(self, parent=None, *args, **kwargs):
@@ -78,4 +68,7 @@ if __name__=="__main__":
     app = QtWidgets.QApplication(sys.argv)
     myapp = MyWin()
     myapp.show()
-    sys.exit(app.exec_())
+    try:
+        sys.exit(app.exec_())
+    except SystemExit:
+        pass
--- math_part.py
+++ math_part.py
@@ -12,7 +12,19 @@ from main import MyWin
 from main import second_window
 
 class mathpart(Ui_MainWindow):
-    def building(self, p, v, y, k, c, u10, u20, eps, d, x0, h):
+    def building(self, p, v, y, k, c, u10, u20, eps, d, x0, h, secwin):
+
+        count_div, count_mul = 0, 0
+        s1, s2 = 0, 0
+        h_list = []
+        S1list, S2list = [], []
+        S1list.append(s1)
+        S2list.append(s2)
+        secwin.tableWidget.setItem(0, 6, QtWidgets.QTableWidgetItem(str(h)))
+        secwin.tableWidget.setItem(0, 1, QtWidgets.QTableWidgetItem(str(x0)))
+        secwin.tableWidget.setItem(0, 2, QtWidgets.QTableWidgetItem(str(u10)))
+        secwin.tableWidget.setItem(0, 3, QtWidgets.QTableWidgetItem(str(u20)))
+
         def du1(u1, u2):
             return p + k * (u1**2 /u2) - y * u1
 
@@ -40,56 +52,112 @@ class mathpart(Ui_MainWindow):
             return res
             
 
-        def next_point(x, u1, u2, step):
+        def next_point(x, u1, u2, number_r):
             nonlocal h
+            secwin.tableWidget.setRowCount(number_r+1)
+            x_new = x + h
+            h_list.append(h)
+            K = calc_coef_for_system(du1, du2, u1, u2, h)
 
-            x_new = x + step
+            s1 = h * (2 * K[0][0] - 9 * K[2][0] + 8 * K[3][0] - K[4][0]) / 30
+            s2 = h * (2 * K[0][1] - 9 * K[2][1] + 8 * K[3][1] - K[4][1]) / 30
+            
+            if self.checkBox.isChecked():
+                secwin.tableWidget.setItem(number_r, 4, QtWidgets.QTableWidgetItem(str(abs(s1))))
+                secwin.tableWidget.setItem(number_r, 5, QtWidgets.QTableWidgetItem(str(abs(s2))))
 
-            K = calc_coef_for_system(du1, du2, u1, u2, step)
+            
+            u1_new = u1 + h * (K[0][0] + 4 * K[3][0] + K[4][0]) / 6
+            u2_new = u2 + h * (K[0][1] + 4 * K[3][1] + K[4][1]) / 6
 
-            s1 = step * (2 * K[0][0] - 9 * K[2][0] + 8 * K[3][0] - K[4][0]) / 30
-            s2 = step * (2 * K[0][1] - 9 * K[2][1] + 8 * K[3][1] - K[4][1]) / 30
+            secwin.tableWidget.setItem(number_r, 0, QtWidgets.QTableWidgetItem(str(number_r)))
+            secwin.tableWidget.setItem(number_r, 1, QtWidgets.QTableWidgetItem(str(x_new)))
+            secwin.tableWidget.setItem(number_r, 2, QtWidgets.QTableWidgetItem(str(u1_new)))
+            secwin.tableWidget.setItem(number_r, 3, QtWidgets.QTableWidgetItem(str(u2_new)))
 
-            u1_new = u1 + step * (K[0][0] + 4 * K[3][0] + K[4][0]) / 6
-            u2_new = u2 + step * (K[0][1] + 4 * K[3][1] + K[4][1]) / 6
-
+            nonlocal count_div, count_mul
             if self.checkBox.isChecked():
                 if abs(s1) >= eps/16 and abs(s2) >= eps/16 and abs(s1) <= eps and abs(s2) <= eps:
+                    S1list.append(abs(s1))
+                    S2list.append(abs(s2))
                     return x_new, u1_new, u2_new
                 elif abs(s1) > eps or abs(s2) > eps:
-                    step /= 2
-                    return next_point(x, u1, u2, step)
+                    count_div += 1
+                    h /= 2
+                    return next_point(x, u1, u2, number_r)
                 elif abs(s1) < eps/16 and abs(s2) < eps/16:
-                    step *= 2
+                    count_mul += 1
+                    h *= 2
+                    S1list.append(abs(s1))
+                    S2list.append(abs(s2))
                     return x_new, u1_new, u2_new
                 else: 
+                    S1list.append(abs(s1))
+                    S2list.append(abs(s2))
                     return x_new, u1_new, u2_new
                     
             else: 
+                
                 return x_new, u1_new, u2_new
 
         self.progressBar.setMinimum(x0)
         self.progressBar.setMaximum(100)
-        ax_1 = self.figure.add_subplot(111)
-        ax_2 = self.figure2.add_subplot(111)
-        #if self.checkBox2.isChecked():
-        #    ax_1.clear()
-        #    ax_2.clear()
-        ax_1.axis([-5, 10, -5, 20])
-        ax_2.axis([-5, 10, -5, 20])
+        ax_1 = self.figure.add_subplot(211)
+        ax_2 = self.figure.add_subplot(212)
+        if self.checkBox_2.isChecked():
+            ax_1.clear()
+            ax_2.clear()
+        ax_1.axis([-1, 50, -1, 2])
+        ax_2.axis([-1, 50, -1, 2])
         v1, v2 = u10, u20
         x = x0
         i = 0
+        secwin.label.setText("Начальное время = " + str(x))
+        secwin.label_2.setText("Начальная конц. активатора = " + str(v1))
+        secwin.label_3.setText("Начальная конц. ингибитора = " + str(v2))
+        secwin.label_4.setText("Плотность активатора k = " + str(p))
+        secwin.label_5.setText("Скорость самообразования активатора k = " + str(k))
+        secwin.label_6.setText("скорость самообразования ингибитора c = " + str(c))
+        secwin.label_7.setText("Естественный распад активатора v = " + str(v))
+        secwin.label_8.setText("Естественный распад ингибитора y = " + str(y))
+        secwin.label_9.setText("Контроль локальной погрешности Eps = " + str(eps))
+        
+        ax_1.set_ylabel("Inhibitor")
+        ax_2.set_ylabel("Activator")
+        ax_2.set_xlabel("Concetnration")
+        xlist, u1list, u2list = [], [], []
+        u1list.append(u10)
+        u2list.append(u20)
+        xlist.append(x0)
         while x < d:
-            i += 1
-            x_old, v1_old, v2_old = x, v1, v2
-            x, v1, v2 = next_point(x, v1, v2, h)
+            x, v1, v2 = next_point(x, v1, v2, i + 1)                    
+            secwin.tableWidget.setItem(i + 1, 6, QtWidgets.QTableWidgetItem(str(h)))
+            secwin.tableWidget.setItem(i + 1, 7, QtWidgets.QTableWidgetItem(str(count_div)))
+            secwin.tableWidget.setItem(i + 1, 8, QtWidgets.QTableWidgetItem(str(count_mul)))           
             self.progressBar.setValue(i)
-            ax_1.plot([x_old, x], [v1_old, v1], '-r')
-            ax_2.plot([x_old, x], [v2_old, v2], '-r')
+            xlist.append(x)
+            u1list.append(v1)
+            u2list.append(v2)
+            i += 1
+        if self.checkBox.isChecked():
+            ax_1.plot(xlist, u1list, '-b', label = 'С контролем ЛП')
+        else:
+            ax_1.plot(xlist, u1list, '-y')
+        if self.checkBox.isChecked():
+            ax_2.plot(xlist, u2list, '-r', label = 'С контролем ЛП')
+        else:
+            ax_2.plot(xlist, u2list, '-y')
+        if self.checkBox.isChecked():
+            secwin.label_10.setText("Максимальная оценка ЛП 1 = " + str(round(max(S1list), 9)))
+            secwin.label_11.setText("Максимальная оценка ЛП 2 = " + str(round(max(S2list), 9)))
+            secwin.label_14.setText("Делений шага = " + str(count_div))
+            secwin.label_15.setText("Удвоений шага = " + str(count_mul))
+        secwin.label_12.setText("Максимальный шаг = " + str(max(h_list)))
+        secwin.label_13.setText("Минимальный шаг = " + str(min(h_list)))
+        ax_1.legend()
+        ax_2.legend()
         ax_1.grid(True)
         ax_2.grid(True)
         self.canvas.draw()
-        self.canvas2.draw()
         
         
